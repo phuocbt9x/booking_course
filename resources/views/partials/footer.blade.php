5fdffd6f5b29336bac9591694f7ced28bb8502bc
Tien Phuong <phuonglt@deha-soft.com>
2025-02-15 20:50:50 +0700

    custom layout

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,90 @@
+ <!-- ========== Footer Area Start============= -->
+ <footer>
+     <div class="container">
+         <div class="row pt-90 pb-90 justify-content-center">
+             <div class="col-lg-3 col-sm-6 order-lg-1 order-2 d-flex justify-content-sm-start justify-content-start">
+                 <div class="footer-items contact ">
+                     <h3>Contacts</h3>
+                     <div class="hotline mb-30">
+                         <div class="hotline-icon">
+                             <img src="assets/images/icon/phone-icon.svg" alt="">
+                         </div>
+                         <div class="hotline-info">
+                             <h6 class="mb-10"><a href="tel:+8801761111456">+880 176 1111 456</a></h6>
+                             <h6><a href="tel:+8801701111000">+880 170 1111 000</a></h6>
+                         </div>
+                     </div>
+                     <div class="email mb-30">
+                         <div class="email-icon">
+                             <img src="assets/images/icon/envelope.svg" alt="">
+                         </div>
+                         <div class="email-info">
+                             <h6 class="mb-10"><a
+                                     href="https://demo.egenslab.com/cdn-cgi/l/email-protection#066f68606946637e676b766a632865696b"><span
+                                         class="__cf_email__"
+                                         data-cfemail="573e39313817322f363a273b327934383a">[email&#160;protected]</span></a>
+                             </h6>
+                             <h6><a
+                                     href="https://demo.egenslab.com/cdn-cgi/l/email-protection#d0b9beb6bf90a3a5a0a0bfa2a4feb3bfbd"><span
+                                         class="__cf_email__"
+                                         data-cfemail="2841464e47685b5d5858475a5c064b4745">[email&#160;protected]</span></a>
+                             </h6>
+                         </div>
+                     </div>
+                     <div class="email">
+                         <div class="email-icon">
+                             <img src="assets/images/icon/location.svg" alt="">
+                         </div>
+                         <div class="email-info">
+                             <h6 class="mb-10"><a>168/170, Avenue 01, Mirpur</a></h6>
+                             <h6><a>DOHS, Bangladesh</a></h6>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <div
+                 class="col-lg-6 d-flex align-items-center order-lg-2 order-1 justify-content-sm-center justify-content-start">
+                 <div class="footer-items">
+                     <h2>Muốn <span>trở thành</span><br>
+                         nhà lãnh đạo, <span>hãy bắt đầu ngay</span>?</h2>
+                     <div class="book-btn2 d-flex justify-content-center text-center">
+                         <a class="primary-btn2" href="contact.html">Đăng ký ngay</a>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-lg-3 col-sm-6 d-flex justify-content-sm-end justify-content-start order-3">
+                 <div class="footer-items opening-time">
+                     <h3>Opening Hours</h3>
+                     <h6 class="mb-25">Mon - Fri: 9.00AM - 6.00PM</h6>
+                     <h6 class="mb-25">Saturday: 9.00AM - 6.00PM</h6>
+                     <h6>Sunday: Closed</h6>
+
+                     <ul class="social-icons">
+                         <li><a href="https://www.facebook.com/"><i class="bx bxl-facebook"></i></a></li>
+                         <li><a href="https://twitter.com/"><i class="bx bxl-twitter"></i></a></li>
+                         <li><a href="https://www.pinterest.com/"><i class="bx bxl-pinterest-alt"></i></a></li>
+                         <li><a href="https://www.instagram.com/"><i class="bx bxl-instagram"></i></a></li>
+                     </ul>
+                 </div>
+             </div>
+         </div>
+         <div class="row border-top">
+             <div class="col-lg-6">
+                 <div class="copyright-area">
+                     <p>© 2023 Scooby is Proudly Powered by <a href="https://www.egenslab.com/">Coaching.</a>
+                     </p>
+                 </div>
+             </div>
+             <div class="col-lg-6 d-flex justify-content-lg-end justify-content-center">
+                 <ul class="footer-btm-menu">
+                     <li><a href="#">Privacy Policy</a></li>
+                     <li><a href="#">Terms & Conditions</a></li>
+                     <li><a href="#">Services</a></li>
+                     <li><a href="#">Help</a></li>
+                 </ul>
+             </div>
+         </div>
+     </div>
+
+ </footer>
+ <!-- ========== Footer Area end============= -->
