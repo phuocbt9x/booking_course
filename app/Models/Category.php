979db8790b53f225ca07b90286ec879b96dcdf1b
phuocbt <phuocbt9x@gmail.com>
2025-02-18 21:52:06 +0700

    build crud category post

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,50 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'slug', 'activated', 'created_by', 'updated_by', 'deleted_by'];
+
+    protected $casts = [
+        'activated' => 'boolean',
+    ];
+
+    protected static function booted()
+    {
+        static::creating(function ($category) {
+            if (!$category->slug) {
+                $category->slug = static::generateUniqueSlug($category->name);
+            }
+            $category->created_by = auth()->id();
+            $category->created_at = now();
+        });
+
+        static::updating(function ($category) {
+            if (!$category->slug || $category->isDirty('name')) {
+                $category->slug = static::generateUniqueSlug($category->name);
+            }
+            $category->updated_by = auth()->id();
+            $category->updated_at = now();
+        });
+
+        static::deleting(function ($category) {
+            $category->deleted_by = auth()->id();
+            $category->save();
+        });
+    }
+
+    protected static function generateUniqueSlug($name): string
+    {
+        $slug = Str::slug($name);
+        $count = Category::where('slug', 'LIKE', "$slug%")->count();
+        return $count > 0 ? "$slug-" . ($count + 1) : $slug;
+    }
 }
+
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -55,8 +54,4 @@ class User extends Authenticatable
         ];
     }
 
-    public function password(): Attribute
-    {
-        return Attribute::set(fn(string $value) => bcrypt($value));
-    }
 }
